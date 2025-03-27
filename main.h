464e809f372beec7d815e727d6b3131001e600a2
Federico Paganini <10647@holbertonstudents.com>
2025-03-27 15:27:40 +0000

    [FIX] Name, prototypes and additions

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stddef.h>
+#include <stdarg.h>
+
+int _printf(const char *, ...);
+
+/**
+ * struct id - Struct id to find.
+ *
+ * @id: The operator
+ * @f: The function associated
+ */
+
+typedef struct id
+{
+	char id;
+	int (*f)(va_list *);
+} id_f;
+
+int (*get_op_func(const char *))(va_list *);
+int _putchar(char);
+int _print_char(va_list *);
+int _print_str(va_list *);
+int _str_len(char *);
+
+#endif
