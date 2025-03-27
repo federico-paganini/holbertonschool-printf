a865e8e72d6d3b815a5e9720c42aac57ee61cb53
Federico Paganini <10647@holbertonstudents.com>
2025-03-27 15:29:29 +0000

    [FEAT] Functions: _str_len, _print_str

---
+++ string_manipulation.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _print_char - Writes the character passed to func to stdout.
+ *
+ * @arg: List of arguments from variadic _printf.
+ *
+ * Return: On success 1. If NULL passed, 6.
+ */
+
+int _print_char(va_list *arg)
+{
+	int *ptr = va_arg(*arg, int *);
+
+	if (ptr != NULL)
+	{
+		return (write(1, ptr, 1));
+	}
+	else
+	{
+		return (write(1, "(null)", 6));
+	}
+}
+
+/**
+ * _print_str - Prints a string to stdout.
+ *
+ * @arg: List of arguments from variadic _printf.
+ *
+ * Return: The number of characters printed or 6 if the string is NULL.
+ */
+
+int _print_str(va_list *arg)
+{
+	char *ptr = va_arg(*arg, char *);
+
+	if (ptr != NULL)
+	{
+		return (write(1, ptr, _str_len(ptr)));
+	}
+	else
+	{
+		return (write(1, "(null)", 6));
+	}
+}
+
+/**
+ * _str_len - Calculates the length of a string.
+ *
+ * @str: String to evaluate.
+ *
+ * Return: The length of the string.
+ */
+
+int _str_len(char *str)
+{
+	unsigned int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	return (length);
+}
